5ea86ddae0cf3854568d88e335b3eba81b464afc
SalCoombs <sal@waldegrave.org>
2025-08-22 10:52:38 -0700

    renamged flowerManager to plantManager.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import EnemyManager from "./enemyManager.js";
 import EventSystem from "./eventSystem.js";
 import KeyHandler from "./keyHandler.js";
 import Player from "./player.js";
-import FlowerManager from "./flowerManager.js";
+import PlantManager from "./plantManager.js";
 
 window.onload = initGame;
 
 function initGame() {
   const eventSystem = new EventSystem();
   const enemyManager = new EnemyManager(eventSystem);
-  const flowerManager = new FlowerManager(eventSystem);
+  const flowerManager = new PlantManager(eventSystem);
   const keyHandler = new KeyHandler(eventSystem);
   const player = new Player(eventSystem);
 
