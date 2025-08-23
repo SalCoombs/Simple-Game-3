35b8489013f5fd0bf3b202838be723860f78494f
SalCoombs <sal@waldegrave.org>
2025-08-23 07:50:08 -0700

    Moved base game initilization to game.js

--- src/game.js
+++ src/game.js
@@ -1,6 +1,17 @@
+import EnemyManager from "./enemyManager.js";
+import KeyHandler from "./keyHandler.js";
+import Player from "./player.js";
+import PlantManager from "./plantManager.js";
+
 export default class Game {
   constructor(eventSystem) {
     this.eventSystem = eventSystem;
+
     this.isRunning = true;
+
+    this.enemyManager = new EnemyManager(this.eventSystem);
+    this.flowerManager = new PlantManager(this.eventSystem);
+    this.keyHandler = new KeyHandler(this.eventSystem);
+    this.player = new Player(this.eventSystem);
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,8 @@
-import EnemyManager from "./enemyManager.js";
+import Game from "./game.js";
 import EventSystem from "./eventSystem.js";
-import KeyHandler from "./keyHandler.js";
-import Player from "./player.js";
-import PlantManager from "./plantManager.js";
 
-window.onload = initGame;
-
-function initGame() {
+window.onload = () => {
   const eventSystem = new EventSystem();
-  const enemyManager = new EnemyManager(eventSystem);
-  const flowerManager = new PlantManager(eventSystem);
-  const keyHandler = new KeyHandler(eventSystem);
-  const player = new Player(eventSystem);
-
-  function runGame(timeStamp) {
-    if (!checkAlive()) {
-      console.log(`You lost the game.`);
-      return;
-    }
-    requestAnimationFrame(runGame);
-  }
-
-  function checkAlive() {
-    if (player.inventory["energy"] <= 0 && flowerManager.entities.length <= 0) {
-      return false;
-    }
-    return true;
-  }
-
-  requestAnimationFrame(runGame);
-}
+  const game = new Game(eventSystem);
+  game.startGame();
+};
