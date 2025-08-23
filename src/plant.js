734ab6b87d9cc4eeb05c73072951daae0467c651
SalCoombs <sal@waldegrave.org>
2025-08-23 05:16:39 -0700

    Plants can now lose life and are on their way to being able to generate energy.

--- src/plant.js
+++ src/plant.js
@@ -1,14 +1,34 @@
 import { eventTypes } from "./constants.js";
 import Entity from "./entity.js";
+import { getRandomNumBetween } from "./util.js";
 
 export default class Plant extends Entity {
   constructor(eventSystem) {
     super(eventSystem, 3);
 
-    const unsub = this.eventSystem.on(
-      eventTypes.PLAYER_ATTACK,
+    let unsub = this.eventSystem.on(
+      eventTypes.ENEMY_ATTACK,
       this.loseLife.bind(this)
     );
     this.unsubscribeFns.push(unsub);
+
+    unsub = this.eventSystem.on(
+      eventTypes.HARVEST_PLANT,
+      this.genEnergy.bind(this)
+    );
+    this.unsubscribeFns.push(unsub);
+  }
+
+  loseLife(damage) {
+    this.life -= damage;
+    console.log(`Plant lost ${damage} life`);
+    this.eventSystem.emit(eventTypes.PLANT_TAKE_DAMAGE, false, this);
+  }
+
+  genEnergy() {
+    const randomEnergy = getRandomNumBetween(1, 3);
+    console.log(`Plant generated ${randomEnergy} energy`);
+
+    this.eventSystem.emit(eventTypes.PLANT_GEN_ENERGY, false, randomEnergy);
   }
 }
