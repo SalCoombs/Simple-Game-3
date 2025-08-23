60d450a3bdaadc0188949d9e0116641b1d6e7098
SalCoombs <sal@waldegrave.org>
2025-08-23 05:17:57 -0700

    Added random function to util.js.

--- src/util.js
+++ src/util.js
@@ -2,3 +2,8 @@ export function getRandomTimeBetween(start, end) {
   const timeDifference = (end - start) * 1000;
   return Math.random() * timeDifference + start * 1000;
 }
+
+export function getRandomNumBetween(low, high) {
+  const timeDifference = end - start;
+  return Math.random() * timeDifference + start;
+}
